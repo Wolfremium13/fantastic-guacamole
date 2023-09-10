c9b59d4844b815973ad0989354592f9e4b4c3a10
Wolfremium13 <wolfremiuminformatica@gmail.com>
2023-09-10 09:56:16 +0100

    🧪 github example

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,24 @@
 import { mdToPdf } from 'md-to-pdf';
-import { FileSystemLocalRepository } from './core/file-system/infrastructure/filesystem.local.repository';
-
-
-const fileSystem = new FileSystemLocalRepository();
-
 
 (async () => {
-	const pdf = await mdToPdf({ path: 'README.md' }).catch(console.error);
-
-	if (pdf) {
-		fileSystem.writeFile('test.pdf', pdf.content);
-	}
+	await mdToPdf(
+		{ path: 'README.md' },
+		{
+			dest: 'test.pdf',
+			stylesheet_encoding: 'utf-8',
+			stylesheet: ['https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/4.0.0/github-markdown.min.css'],
+            body_class: ['markdown-body'],
+            highlight_style: 'github',
+			pdf_options: {
+				format: 'A4',
+				printBackground: true,
+				margin: {
+					top: '2cm',
+					bottom: '2cm',
+					left: '2cm',
+					right: '2cm',
+				},
+			},
+		}
+	).catch(console.error);
 })();
